022f0a6d3b4835ae1b002694a5bfdbe05c22c412
Vlaska <vlaska8888@gmail.com>
2019-08-02 12:18:24 +0200

    Usunięto *.d.ts

--- types/Matrix.d.ts
+++
@@ -1,5 +0,0 @@
-import { vec2 } from './Base';
-export declare type Matrix = Array<number> | Float32Array;
-export declare function computeMatrix(_in: Matrix, out: Matrix, pos: vec2, scale: vec2, trans: vec2, angle: number): Matrix;
-export declare function projectionMatrix(out: Matrix, width: number, height: number): Matrix;
-export declare function createProjection(width: number, height: number): Matrix;
